33db13ae79d9496d9314f8b3e5080a61f3287b37
ImSpaghettio <aburroughs00@live.com>
2016-11-14 15:48:59 -0500

    Starting ScreenManager

--- Platformer/ScreenManager.cs
+++ Platformer/ScreenManager.cs
@@ -8,12 +8,32 @@ namespace Platformer
 {
     public class ScreenManager
     {
+        Screen screen;
+
+        public void Initialize()
+        {
+            screen = Screen.TitleScreen;
+        }
+
         public void LoadContent()
+        {
+            switch (screen)
+            {
+                case Screen.TitleScreen:
+                    break;
+                case Screen.GameScreen:
+                    break;
+                case Screen.LoadingScreen:
+                    break;
+            }
+        }
+
+        public void Update()
         {
 
         }
 
-        public void Update()
+        public void Draw()
         {
 
         }
